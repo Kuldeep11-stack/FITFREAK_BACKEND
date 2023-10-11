1919dc37b6724b2d67bb7d33dd979dc4621a73b4
Kuldeep <kuldeepmer11@gmail.com>
2023-10-12 00:50:29 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    dbName : process.env.DB_NAME
+}).then(
+    () => {
+        console.log('DB connected');
+    }
+).catch(
+    (err) => {
+        console.log('Error connecting to DB' + err);
+    }
+)
